e5370e59e43d27a722e073903460af88ea23039f
Katie Schick <kkschick@gmail.com>
2016-08-13 19:48:15 -0400

    Clean up form

--- public/components/rsvp.js
+++ public/components/rsvp.js
@@ -3,7 +3,10 @@ import React from 'react';
 export default class RSVP extends React.Component {
   render() {
     return (
-      <h1 className="header-large">RSVP</h1>
+      <div>
+        <h1 className="header-large">RSVP</h1>
+        <div className="paragraph-text">Coming soon!</div>
+      </div>
     );
   }
 }
