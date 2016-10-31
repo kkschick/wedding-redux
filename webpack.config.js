dda2bb23fed823f84710823f1f208432df84e452
Katie Schick <kkschick@gmail.com>
2016-10-31 14:08:12 -0400

    Uglify

--- webpack.config.js
+++ webpack.config.js
@@ -23,6 +23,11 @@ module.exports = {
   },
   plugins: [
     new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false
+      }
+    }),
     new webpack.HotModuleReplacementPlugin(),
     new webpack.NoErrorsPlugin()
   ],
